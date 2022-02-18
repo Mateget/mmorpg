6e5f3d24a8935654d9095d6520ec9767b1013099
Zanckor <47495541+Zanckor@users.noreply.github.com>
2022-02-18 18:19:01 +0100

    Add files via upload

--- src/main/java/com/mod/mmorpgmod/MMORPGZanckorMod.java
+++ src/main/java/com/mod/mmorpgmod/MMORPGZanckorMod.java
@@ -5,14 +5,13 @@ import org.apache.logging.log4j.Logger;
 
 import com.mod.capabilities.MyCapability;
 import com.mod.capabilities.MyCapabilityImplementation;
-import com.mod.capabilities.MyCapabilityStorage;
+import com.mod.capabilities.MyCapabilityProvider;
 import com.mod.items.ItemInit;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("mmorpg")
@@ -30,8 +29,8 @@ public class MMORPGZanckorMod {
         
     }
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void onCommonSetup(FMLCommonSetupEvent event) {
-	    CapabilityManager.INSTANCE.register(MyCapability.class, new MyCapabilityStorage(), MyCapabilityImplementation::new);
+	
+	public static void registerCapabilities() {
+		CapabilityManager.INSTANCE.register(MyCapability.class, null, MyCapabilityImplementation.class);
 	}
 }
